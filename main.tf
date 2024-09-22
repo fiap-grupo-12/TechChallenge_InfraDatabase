9ae0384da187fbab6b6e8e6ad1f1f70bcdd7e916
fiap-grupo-12 <fiapgrupo12@outlook.com>
2024-09-22 17:30:49 -0300

    Removendo o security group

--- main.tf
+++ main.tf
@@ -12,27 +12,6 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
-resource "aws_security_group" "rds_security_group" {
-  name        = "rds-security-group"
-  description = "Security group for RDS"
-
-  // Regra permitindo tráfego de entrada de qualquer origem
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Regra padrão permitindo todo tráfego de saída
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_db_instance" "fiap-tech-challenge-rds" {
   engine = "sqlserver-ex"
   engine_version = "15.00.4385.2.v1"
@@ -45,7 +24,6 @@ resource "aws_db_instance" "fiap-tech-challenge-rds" {
   password = var.db_password
   publicly_accessible = true
   skip_final_snapshot = true
-  vpc_security_group_ids = [aws_security_group.rds_security_group.id]
 
   tags = {
     Name = "fiap-techchallenge"
